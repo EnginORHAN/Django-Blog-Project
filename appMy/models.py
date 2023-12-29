d74c9e650c23f39d4815f76e145b175d20ad4097
engin-orhan <enginorhan2000@gmail.com>
2023-12-29 14:53:43 +0300

    first commit

---
+++ appMy/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+
+#model tablosu oluşturmak için 
+#python manage.py makemigrations
+#python manage.py migrate
+#SQL database içerisine tablooluşturur
+
+class Blog(models.Model):
+    title = models.CharField(("Başlık"), max_length=50)
+    text = models.TextField()
+    date_now= models.DateTimeField(("Tarih - Saat"), auto_now=False, auto_now_add=False)
+    author = models.CharField(("Yazar"), max_length=50)
+
